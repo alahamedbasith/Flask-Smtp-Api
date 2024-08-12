c2ce46f87435d3c08bdc045cd347bf3102a71e11
Ahamed Basith <81899645+alahamedbasith@users.noreply.github.com>
2024-08-12 18:58:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, request, jsonify
 from flask_mail import Mail, Message
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Configuring the Flask-Mail
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'alahamedbasithce@gmail.com'  # Your email address
-app.config['MAIL_PASSWORD'] = 'amnbzamsacwumroy'  # Your email password
-app.config['MAIL_DEFAULT_SENDER'] = 'alahamedbasithce@gmail.com'
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')  # Use environment variable
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')  # Use environment variable
+app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
 
 mail = Mail(app)
 
@@ -31,7 +36,7 @@ def contact_post():
     print(f"Name: {name}, Email: {email}, Message: {message}")
 
     msg = Message('Contact Form Submission',
-                  recipients=['ahamedbasith006@gmail.com'])
+                  recipients=[os.getenv('MAIL_USERNAME')])
     msg.body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
 
     try:
